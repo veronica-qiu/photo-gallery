a9156b27e7f3224e461ecc17fd517f1d6de8722d
veronica-qiu <friendercoolevents@gmail.com>
2022-07-30 07:24:21 +0200

    taking pictures and viewing them on the grid

---
+++ src/app/services/photo.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
+import { Filesystem, Directory } from '@capacitor/filesystem';
+import { Storage } from '@capacitor/storage';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class PhotoService {
+  public photos: UserPhoto[] = [
+    
+  ];
+
+public async addNewToGallery() {
+  
+
+  
+  // Take a photo
+  const capturedPhoto = await Camera.getPhoto({
+    resultType: CameraResultType.Uri,
+    source: CameraSource.Camera,
+    quality: 100
+  });
+  
+  this.photos.unshift({
+    filepath: "soon...",
+    webviewPath: capturedPhoto.webPath
+  });
+}
+  constructor() { }
+}
+export interface UserPhoto {
+  filepath: string;
+  webviewPath: string;
+}
